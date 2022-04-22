d13ce20783b2765035f8331cf641b58e1b973a7c
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-22 10:05:09 +0200

    acabados los percentiles y todo el ejercicio

--- clases/critica.py
+++ clases/critica.py
@@ -24,7 +24,6 @@ class Grafica:
         media = round((suma_producto/suma_frecuencia), 2)
         for k in range (len(lista_opinion)):
             suma_varianza += lista_votantes[k]*((lista_opinion[k] - media)**2)
-            print(suma_varianza)
         media = round((suma_producto/suma_frecuencia), 2)
         varianza = round((suma_varianza/suma_frecuencia), 2)
         desviacion_tipica = round((math.sqrt(varianza)), 2)
@@ -44,10 +43,19 @@ class Grafica:
         for j in lista_votantes:
             suma_frecuencia += j
         intervalo = (95 * suma_frecuencia)/100
-        print(f"El 95% de los datos son {intervalo}")
-        l_inferior = 4 ; suma_frecuencia_anterior = 414 ; frecuencia_intervalo = 99 ; amplitud = 1  #Datos sacados de la tabla
+        print(f"\nEl 95% de los datos son {intervalo}")
+        l_inferior = 3 ; suma_frecuencia_anterior = 414 ; frecuencia_intervalo = 99 ; amplitud = 1  #Datos sacados de la tabla
         percentil_95 = round(l_inferior + (((intervalo - suma_frecuencia_anterior)/frecuencia_intervalo)* amplitud), 3)
         print(f"El percentil 68% es {percentil_95}")
+    def percentil_97():
+        lista_votantes = list(datos["Cantidad de votantes"]) ; suma_frecuencia = 0
+        for j in lista_votantes:
+            suma_frecuencia += j
+        intervalo = (97 * suma_frecuencia)/100
+        print(f"\nEl 97% de los datos son {intervalo}")
+        l_inferior = 4 ; suma_frecuencia_anterior = 513 ; frecuencia_intervalo = 40 ; amplitud = 1  #Datos sacados de la tabla
+        percentil_98 = round(l_inferior + (((intervalo - suma_frecuencia_anterior)/frecuencia_intervalo)* amplitud), 3)
+        print(f"El percentil 68% es {percentil_98}")
 
 
 def elegir_subejercicio():
@@ -60,6 +68,7 @@ def elegir_subejercicio():
     elif enunciado == 3:
         Grafica.percentil_68()
         Grafica.percentil_95()
+        Grafica.percentil_97()
     elif enunciado == 4:
         exit()
     else:
