18f66a52670488cea70ca842803e62cf91a3fc18
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-26 10:08:45 +0200

    planteado la mejora del ejercicio

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-from clases import critica
+import pandas as pnd
+import Clases_Rubén.JMPEstadisticas as jmp
+import numpy as np
+
+from Clases import notas
 if __name__ == '__main__':
-    critica.elegir_subejercicio()
+    eleccion = int(input(("¿Qué versión quieres usar? \n 1: La creada anterior a las nuevas pautas \n 2: La nueva dada por las nuevas pautas\n")))
+    if eleccion == 1:
+        notas.iniciar()
+    elif eleccion ==2:
+        datos = pnd.read_csv("Student_grade.csv", header=0 , sep =",")
+        lista_notas = list(datos["Notas"])
+        observaciones = pnd.DataFrame({'NOTAS': lista_notas})
+        #--- ANALISIS DE UNA CARACTERISTICA ---
+        stats = jmp.JMPEstadisticas(observaciones['NOTAS'])
+        stats.analisisCaracteristica()
