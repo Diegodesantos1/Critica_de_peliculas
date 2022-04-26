021f8e3b9d5bacc61e446f4931ec4e2bf7003200
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-26 10:11:04 +0200

    falta optimizarlo

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import pandas as pnd
 import Clases_Rubén.JMPEstadisticas as jmp
 import numpy as np
 
-from Clases import notas
+from Clases import critica
 if __name__ == '__main__':
     eleccion = int(input(("¿Qué versión quieres usar? \n 1: La creada anterior a las nuevas pautas \n 2: La nueva dada por las nuevas pautas\n")))
     if eleccion == 1:
-        notas.iniciar()
+        critica.iniciar()
     elif eleccion ==2:
-        datos = pnd.read_csv("Student_grade.csv", header=0 , sep =",")
-        lista_notas = list(datos["Notas"])
+        datos = pnd.read_csv("imbd_sup.csv", header=0 , sep =",")
+        lista_notas = list(datos["Rating"])
         observaciones = pnd.DataFrame({'NOTAS': lista_notas})
         #--- ANALISIS DE UNA CARACTERISTICA ---
         stats = jmp.JMPEstadisticas(observaciones['NOTAS'])
